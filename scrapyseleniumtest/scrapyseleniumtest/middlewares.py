654bed037b128b61f66c600ae34e1953c1df3c76
hamdi <javs_shao@163.com>
2018-12-23 15:44:44 +0800

    对接Selenium

--- scrapyseleniumtest/scrapyseleniumtest/middlewares.py
+++ scrapyseleniumtest/scrapyseleniumtest/middlewares.py
@@ -11,9 +11,42 @@ class SeleniumMiddleware():
     def __init__(self, timeout=None, service_args=[]):
         self.logger = getLogger(__name__)
         self.timeout = timeout
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.PhantomJS(service_args=service_args)
         self.browser.set_window_size(1400, 700)
         self.wait = WebDriverWait(self.browser, self.timeout)
 
     def __del__(self):
         self.browser.close()
+
+    def process_request(self, request, spider):
+        """
+        使用PhantomJS抓取页面
+        :param request:
+        :param spider:
+        :return:
+        """
+        self.logger.debug('phantomjs已经开始运行了....')
+        page = request.meta.get('page', 1)
+        try:
+            self.browser.get(request.url)
+            if page > 1:
+                input = self.wait.until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input')))
+                submit = self.wait.until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager div.form > span.btn.J_Submit')))
+                input.clear()
+                input.send_keys(page)
+                submit.click()
+            self.wait.until(
+                EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager li.item.active > span'), str(page)))
+            self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
+            return HtmlResponse(url=request.url, body=self.browser.page_source, request=request, encoding='utf-8',
+                                status=200)
+        except TimeoutException:
+            return HtmlResponse(url=request.url, status=500, request=request)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(timeout=crawler.settings.get('SELENIUM_TIMEOUT'),
+                   service_args=crawler.settings.get('PHANTOMJS_SERVICE_ARGS')
+                   )
