a4eedf51266c671cd592ba02c0314acebf2a071f
hamdi <javs_shao@163.com>
2018-12-23 16:32:06 +0800

    配置文件_ITEM_PIELINES

--- scrapyseleniumtest/scrapyseleniumtest/settings.py
+++ scrapyseleniumtest/scrapyseleniumtest/settings.py
@@ -64,9 +64,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapyseleniumtest.pipelines.ScrapyseleniumtestPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scrapyseleniumtest.pipelines.MongoPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
