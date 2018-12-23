a82840ddad348e3bffa8a733a1846f2683c769b6
hamdi <javs_shao@163.com>
2018-12-23 16:12:55 +0800

    Item Pipeline_初始化

--- scrapyseleniumtest/scrapyseleniumtest/pipelines.py
+++ scrapyseleniumtest/scrapyseleniumtest/pipelines.py
@@ -1,11 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
 
 
-class ScrapyseleniumtestPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class MongoPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
