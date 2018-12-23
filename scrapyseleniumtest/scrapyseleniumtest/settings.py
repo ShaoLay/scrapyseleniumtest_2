26168e2b93ad34eef9a7fbe5f21413f4671cbe76
hamdi <javs_shao@163.com>
2018-12-23 15:47:38 +0800

    配置文件_设置调用定义的SeleniumMiddleware

--- scrapyseleniumtest/scrapyseleniumtest/settings.py
+++ scrapyseleniumtest/scrapyseleniumtest/settings.py
@@ -52,9 +52,9 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'scrapyseleniumtest.middlewares.ScrapyseleniumtestDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'scrapyseleniumtest.middlewares.SeleniumMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
